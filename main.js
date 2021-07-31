be48c872b1bbedcd65e79f0a6b7f9d6db901c8fc
Jayromulus <jerbear2@gmail.com>
2021-07-31 03:42:08 -0400

    refactored enemy rendering

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const enemies = document.querySelectorAll('.enemy')
+const enemies = document.querySelector('.enemies')
 const hand = document.querySelector('.hand')
 
 //! CURRENT DECK IMAGE DEFINITION
+// this will get replaced with an array of object names that will be used to reference a json using [] during the hand display loop. 
 const deck = [
   './assets/Magma_Morsel.webp',
   './assets/Antumbra_Morsel.webp',
@@ -11,21 +12,34 @@ const deck = [
   './assets/Rubble_Morsel.webp'
 ]
 
+const enemiesList = [
+  './assets/Penumbra.webp',
+  './assets/Penumbra.webp',
+  './assets/Penumbra.webp'
+]
+
+// storage will be used as the "brains" to keep track of the card selected and the enemy targeted by the user.
 let storage = {
   card: -1,
+  enemy: -1
 }
+
+// the proxy is being used as a sort of "event listener" for when the variable changes. in this case it will run a function tha happens when the obeject changes and will act accordingly
+// in this case the for loop and if statement will be resetting the selection availability for each enemy other than the one currently selected by the user. so when a user selects item 0 it will reset numbers 1 and 2, if they pick number 1 then 0 and 2 will get reset
 //* look into more of what the Proxy does
 //* target, function, value === target.key = value
 let storageProxy = new Proxy(storage, {
   set: function (target, key, value) {
-    if(target[key] !== value){
+    if (target[key] !== value && value !== undefined) {
       // will console.log the key and value when it is changed as listened by the proxy
       console.log(`${key} set to ${value}`);
       target[key] = value;
-      for(let i = 0; i < enemies.length; i++){
-        if(i != value){
+      let currentEnemies = document.querySelectorAll('.enemy')
+      // console.log(currentEnemies)
+      for (let i = 0; i < currentEnemies.length; i++) {
+        if (i != value) {
           // console.log('TEST RUN')
-          enemies[i].addEventListener('dragover', e => dragOver(e, i), { once: true })
+          currentEnemies[i].addEventListener('dragover', e => dragOver(e, i), { once: true })
         }
       }
       return true;
@@ -33,7 +47,7 @@ let storageProxy = new Proxy(storage, {
   }
 });
 
-storageProxy.enemy = "test"; // console: 'hello_world set to test'
+// storageProxy.enemy = "test"; // console: 'hello_world set to test'
 
 // create array of card names to reference a json that will have all of the card listed with names, damage, block, description, effects
 //! MAIN DECK LOOP 
@@ -47,20 +61,23 @@ deck.forEach((image, index) => {
 
   current.style.backgroundImage = `url(${image})`
 
-  current.addEventListener('dragstart', e => dragStart(e,index))
+  current.addEventListener('dragstart', e => dragStart(e, index))
   current.addEventListener('dragend', dragEnd)
 
   hand.appendChild(current)
 })
 
-enemies.forEach((enemy, index) => {
-  enemy.addEventListener('dragover', e => dragOver(e,index), { once: true })
+enemiesList.forEach((enemy, index) => {
+  const single = document.createElement('div')
+  single.classList.add('enemy')
+  single.addEventListener('dragover', e => dragOver(e, index), { once: true })
+  enemies.appendChild(single)
   storageProxy.enemy = index;
 })
 
 
 //! FUNCTION ALLEY
-function dragStart(e,i) {
+function dragStart(e, i) {
   // console.log('started drag')
 
   storageProxy.card = i
@@ -68,18 +85,21 @@ function dragStart(e,i) {
 
 function dragEnd(e) {
   // console.log(`ended drag ${storage.card}`)
-
-  enemies.forEach(enemy => {
+  const currentEnemies = document.querySelectorAll('.enemy')
+  currentEnemies.forEach(enemy => {
     enemy.addEventListener('dragover', dragOver, { once: true })
     enemy.style.backgroundColor = 'initial'
   })
+  storageProxy.card = -1
+  storageProxy.enemy = -1
 }
 
-function dragOver(e,i) {
+function dragOver(e, i) {
   // console.log(`testing ${storage.card} dragover`)
   storageProxy.enemy = i
-  enemies.forEach((enemy, index) => {
-    if(index == i){
+  const currentEnemies = document.querySelectorAll('.enemy')
+  currentEnemies.forEach((enemy, index) => {
+    if (index == i) {
       enemy.style.backgroundColor = 'black'
     } else {
       enemy.style.backgroundColor = 'initial'
