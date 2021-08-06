4de6fe18b323d7002734e40a455b565d92979c8b
Jerome Martin Flaherty <jerbear2@gmail.com>
2021-08-06 00:04:25 -0400

    drag and drop refactor thanks adam

--- main.js
+++ main.js
@@ -28,24 +28,24 @@ let storage = {
 // in this case the for loop and if statement will be resetting the selection availability for each enemy other than the one currently selected by the user. so when a user selects item 0 it will reset numbers 1 and 2, if they pick number 1 then 0 and 2 will get reset
 //* look into more of what the Proxy does
 //* target, function, value === target.key = value
-let storageProxy = new Proxy(storage, {
-  set: function (target, key, value) {
-    if (target[key] !== value && value !== undefined) {
-      // will console.log the key and value when it is changed as listened by the proxy
-      console.log(`${key} set to ${value}`);
-      target[key] = value;
-      let currentEnemies = document.querySelectorAll('.enemy')
-      // console.log(currentEnemies)
-      for (let i = 0; i < currentEnemies.length; i++) {
-        if (i != value) {
-          // console.log('TEST RUN')
-          currentEnemies[i].addEventListener('dragover', e => dragOver(e, i), { once: true })
-        }
-      }
-      return true;
-    }
-  }
-});
+// let storageProxy = new Proxy(storage, {
+//   set: function (target, key, value) {
+//     if (target[key] !== value && value !== undefined) {
+//       // will console.log the key and value when it is changed as listened by the proxy
+//       console.log(`${key} set to ${value}`);
+//       target[key] = value;
+//       let currentEnemies = document.querySelectorAll('.enemy')
+//       // console.log(currentEnemies)
+//       for (let i = 0; i < currentEnemies.length; i++) {
+//         if (i != value) {
+//           // console.log('TEST RUN')
+//           currentEnemies[i].addEventListener('dragover', e => dragOver(e, i), { once: true })
+//         }
+//       }
+//       return true;
+//     }
+//   }
+// });
 
 // storageProxy.enemy = "test"; // console: 'hello_world set to test'
 
@@ -62,48 +62,66 @@ deck.forEach((image, index) => {
   current.style.backgroundImage = `url(${image})`
 
   current.addEventListener('dragstart', e => dragStart(e, index))
-  current.addEventListener('dragend', dragEnd)
+  // current.addEventListener('dragend', dragEnd)
 
   hand.appendChild(current)
 })
 
 enemiesList.forEach((enemy, index) => {
   const single = document.createElement('div')
+  single.style.backgroundImage = `url(${enemy})`
   single.classList.add('enemy')
-  single.addEventListener('dragover', e => dragOver(e, index), { once: true })
+  // single.addEventListener('dragover', e => dragOver(e, index), { once: true })
+  single.addEventListener('dragenter', e => {
+    dragEnter(e, index)
+  })
+  single.addEventListener('dragover', e => e.preventDefault())
+  single.addEventListener('drop', e => drop(e))
   enemies.appendChild(single)
-  storageProxy.enemy = index;
+  storage.enemy = index;
 })
 
 
 //! FUNCTION ALLEY
 function dragStart(e, i) {
-  // console.log('started drag')
+  // e.preventDefault()
+  console.log('started drag')
 
-  storageProxy.card = i
+  storage.card = i
 }
 
-function dragEnd(e) {
-  // console.log(`ended drag ${storage.card}`)
-  const currentEnemies = document.querySelectorAll('.enemy')
-  currentEnemies.forEach(enemy => {
-    enemy.addEventListener('dragover', dragOver, { once: true })
-    enemy.style.backgroundColor = 'initial'
-  })
-  storageProxy.card = -1
-  storageProxy.enemy = -1
+function dragEnter(e, i) {
+  e.preventDefault()
+  storage.enemy = i
+  console.log(storage)
 }
 
-function dragOver(e, i) {
-  console.log(e)
-  // console.log(`testing ${storage.card} dragover`)
-  storageProxy.enemy = i
-  const currentEnemies = document.querySelectorAll('.enemy')
-  currentEnemies.forEach((enemy, index) => {
-    if (index == i) {
-      enemy.style.backgroundColor = 'black'
-    } else {
-      enemy.style.backgroundColor = 'initial'
-    }
-  })
+function drop() {
+  console.log(`Card ${storage.card} dropped on enemy ${storage.enemy}`)
 }
+
+
+// function dragEnd(e) {
+//   // console.log(`ended drag ${storage.card}`)
+//   const currentEnemies = document.querySelectorAll('.enemy')
+//   currentEnemies.forEach(enemy => {
+//     enemy.addEventListener('dragover', dragOver, { once: true })
+//     enemy.style.backgroundColor = 'initial'
+//   })
+//   storageProxy.card = -1
+//   storageProxy.enemy = -1
+// }
+
+// function dragOver(e, i) {
+//   console.log(e)
+//   // console.log(`testing ${storage.card} dragover`)
+//   storageProxy.enemy = i
+//   const currentEnemies = document.querySelectorAll('.enemy')
+//   currentEnemies.forEach((enemy, index) => {
+//     if (index == i) {
+//       enemy.style.backgroundColor = 'black'
+//     } else {
+//       enemy.style.backgroundColor = 'initial'
+//     }
+//   })
+// }
